a90bb85a1c5c6a7934597590c503674776da5481
BlackBroke <paradox.ubivaet@mail.ru>
2022-07-03 15:54:08 +0800

    Добавить навигационное меню категорий в представление.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                                  .Select(x => x.Category)
+                                  .Distinct()
+                                  .OrderBy(x => x));
+        }
+    }
+}
